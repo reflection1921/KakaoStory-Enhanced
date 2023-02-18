afd26de0006763ff614f58283daf74acebf74151
Reflection <firesoft1212@gmail.com>
2023-02-18 23:43:21 +0900

    KakaoStory Enhanced v1.12 업데이트

--- enhanced.user.js
+++ enhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KakaoStory Enhanced
 // @namespace    http://chihaya.kr
-// @version      1.11
+// @version      1.12
 // @description  Add-on for KakaoStory
 // @author       Reflection, 박종우
 // @match        https://story.kakao.com/*
@@ -39,7 +39,7 @@
  */
 
 
-let scriptVersion = "1.11";
+let scriptVersion = "1.12";
 
 //let resourceURL = 'http://127.0.0.1:8188/kakaostory-enhanced/'; //for debug
 //let resourceURL = 'https://raw.githubusercontent.com/reflection1921/KakaoStory-Enhanced/dev/'; //github dev
@@ -60,7 +60,7 @@ let jsonMyFriends;
 
 function AddEnhancedMenu() {
     document.getElementsByClassName("menu_util")[0].innerHTML = '<li><a href="#" id="enhancedOpenSettings" class="link_menu _btnSettingProfile">Enhanced 설정</a></li>' + document.getElementsByClassName("menu_util")[0].innerHTML;
-    $('body').on('click', '#enhancedOpenSettings', function() {
+    document.getElementById("enhancedOpenSettings").addEventListener("click", function() {
         document.getElementById("enhancedLayer").style.display = 'block';
         document.body.scrollTop = 0;
         DisableScroll();
