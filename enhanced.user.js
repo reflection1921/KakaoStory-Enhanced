4f512cec31e9da3a03d4aa5425d22be87c260621
Reflection <firesoft1212@gmail.com>
2023-06-06 13:15:41 +0900

    KakaoStory Enhanced v1.19 업데이트

--- enhanced.user.js
+++ enhanced.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KakaoStory Enhanced
 // @namespace    http://chihaya.kr
-// @version      1.18
+// @version      1.19
 // @description  Add-on for KakaoStory
 // @author       Reflection, 박종우
 // @match        https://story.kakao.com/*
@@ -48,7 +48,7 @@
  */
 
 
-let scriptVersion = "1.18";
+let scriptVersion = "1.19";
 
 //let resourceURL = 'http://127.0.0.1:8188/kakaostory-enhanced/'; //for debug
 //let resourceURL = 'https://raw.githubusercontent.com/reflection1921/KakaoStory-Enhanced/dev/'; //github dev
